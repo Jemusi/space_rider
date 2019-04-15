eacf384bba3f543c4f928562755e39a859a80460
James Shen <jamesshen09@gmail.com>
2019-04-14 23:29:07 -0700

    Implemented matrix that rotates asteroid object.

--- app/src/main/java/com/boysintheback/space_rider/Asteroid.java
+++ app/src/main/java/com/boysintheback/space_rider/Asteroid.java
@@ -2,6 +2,7 @@ package com.boysintheback.space_rider;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 
 public class Asteroid extends GameObject {
 
@@ -18,6 +19,9 @@ public class Asteroid extends GameObject {
     }
 
     public void update() {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(1);
+        this.image = Bitmap.createBitmap(this.image,0,0, this.image.getWidth(), this.image.getHeight(), matrix, true);
         y += speed;
     }
 
