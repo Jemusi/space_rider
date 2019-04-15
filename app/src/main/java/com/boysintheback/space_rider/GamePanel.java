c5aefbcabf7af89b182118f30ce2f5f53ff05309
Roy <g5n1b@ugrad.cs.ubc.ca>
2019-04-14 21:47:00 -0700

    added seconds functionality, also completed abstract class implementation of GameObject

--- app/src/main/java/com/boysintheback/space_rider/GamePanel.java
+++ app/src/main/java/com/boysintheback/space_rider/GamePanel.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Point;
+import android.graphics.Point; 
 import android.view.Display;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
